e6e790b8cf883dfed220ed003ef143cd7e249b90
shafik720 <shafikrasel5@gmail.com>
2024-11-06 13:58:12 +0600

    navbar : made mobile menu icons animation more stylish

--- src/app/components/shared/Navbar/Navbar.tsx
+++ src/app/components/shared/Navbar/Navbar.tsx
@@ -3,6 +3,23 @@ import logo from "@/assets/img/logo.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdRestaurantMenu } from "react-icons/md";
+
+interface MenuLink {
+  path: string;
+  label: string;
+  icon?: React.ReactNode;
+}
+
+const menuLink: MenuLink[] = [
+  { path: "/", label: "Home" },
+  { path: "/about", label: "About" },
+  { path: "/portfolio", label: "Portfolio" },
+  { path: "/clients", label: "Clients" },
+  { path: "/blog", label: "Blog" },
+  { path: "/contact", label: "Contact" },
+];
 
 const Navbar = () => {
   const [isSticky, setIsSticky] = useState(false);
@@ -57,18 +74,12 @@ const Navbar = () => {
         isSticky ? "bg-[rgba(189,31,23,0.43)] shadow-md" : "bg-transparent"
       }`}
     >
-      <nav className="flex justify-between items-center max-w-[1320px] mx-auto py-4 px-6">
+      <nav className="flex justify-between items-center max-w-[1320px] mx-auto py-4 w-[90%] ">
+        {/* --- Navbar left --- */}
         <div className="navbar-brand flex justify-center items-center gap-8">
           <Image src={logo} alt="Restaurant Logo" className="w-[200px] " />
           <ul className="hidden md:flex space-x-6 font-extralight text-sm">
-            {[
-              { path: "/", label: "Home" },
-              { path: "/about", label: "About" },
-              { path: "/portfolio", label: "Portfolio" },
-              { path: "/clients", label: "Clients" },
-              { path: "/blog", label: "Blog" },
-              { path: "/contact", label: "Contact" },
-            ].map((item) => (
+            {menuLink.map((item) => (
               <li
                 key={item.path}
                 className={`nav-item ${
@@ -87,32 +98,32 @@ const Navbar = () => {
           <button className="btn-primary">Book a table</button>
         </div>
         <button
-          className="navbar-toggler md:hidden"
+          className="navbar-toggler md:hidden  relative  h-7 w-7 "
           onClick={(e) => handleToggle(e)}
           ref={toggleButtonRef}
         >
-          <span className="block w-6 h-1 bg-white my-1"></span>
-          <span className="block w-6 h-1 bg-white my-1"></span>
-          <span className="block w-6 h-1 bg-white my-1"></span>
+          <GiHamburgerMenu
+            className={`text-white w-7 h-7 absolute top-0 left-0 right-0 bottom-0 ${
+              !isMobileMenuOpen ? "opacity-100" : "opacity-0"
+            } transition-opacity duration-500`}
+          />
+          <MdRestaurantMenu
+            className={`text-white w-7 h-7 absolute top-0 left-0 right-0 bottom-0 ${
+              isMobileMenuOpen ? "opacity-100" : "opacity-0"
+            } transition-opacity duration-400`}
+          />
         </button>
       </nav>
 
       {/* Mobile Slide-In Menu */}
       <div
         ref={sidebarRef}
-        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 transform transition-transform duration-300 ${
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 transform transition-transform duration-300 flex flex-col justify-center items-center ${
           isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <div className="flex flex-col items-start p-4">
-          {[
-            { path: "/", label: "Home" },
-            { path: "/about", label: "About" },
-            { path: "/portfolio", label: "Portfolio" },
-            { path: "/clients", label: "Clients" },
-            { path: "/blog", label: "Blog" },
-            { path: "/contact", label: "Contact" },
-          ].map((item) => (
+        <div className="flex flex-col items-start p-4 ">
+          {menuLink.map((item: MenuLink) => (
             <a
               key={item.path}
               href={item.path}
@@ -124,7 +135,9 @@ const Navbar = () => {
               {item.label}
             </a>
           ))}
-          <button className="btn-primary mt-4">Book a table</button>
+          <button className="btn-primary-small mt-4 rounded-full">
+            Book a table
+          </button>
         </div>
       </div>
     </header>
